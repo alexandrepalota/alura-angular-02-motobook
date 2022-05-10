673c120bb35974e50d05cfe6784146567450cfab
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-10 10:11:32 -0300

    Reactive forms

--- src/app/home/new-user/new-user.component.ts
+++ src/app/home/new-user/new-user.component.ts
@@ -1,4 +1,7 @@
+import { NewUserService } from './new-user.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NewUser } from './new-user';
 
 @Component({
   selector: 'app-new-user',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserComponent implements OnInit {
 
-  constructor() { }
+  newUserForm!: FormGroup
+
+  constructor(private formBuilder: FormBuilder, private newUserService: NewUserService) { }
 
   ngOnInit(): void {
+    this.newUserForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: ['']
+    })
+  }
+
+  register() {
+    const newUser = this.newUserForm.getRawValue() as NewUser
+    console.log(newUser)
   }
 
 }
