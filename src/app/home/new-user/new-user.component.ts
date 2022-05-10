6ae762add4505387c728897991777bf7734e6919
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-10 13:25:12 -0300

    Validation form

--- src/app/home/new-user/new-user.component.ts
+++ src/app/home/new-user/new-user.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NewUser } from './new-user';
 import { lowerCaseValidator } from './lowerCase.validator';
+import { userEqualsPasswordValidator } from './user-equals-password.validator';
 
 @Component({
   selector: 'app-new-user',
@@ -17,7 +18,9 @@ export class NewUserComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private newUserService: NewUserService, private userExistsService: UserExistsService) { }
 
   ngOnInit(): void {
-    this.newUserForm = this.formBuilder.group({
+    this.newUserForm = this.formBuilder.group(
+      // The first group parameter contains the fields validators
+      {
       // To validate a field, we add an array on the second position of original array
       // and use the Angular class Validators, and his validations.
       email: ['', [Validators.required, Validators.email]],
@@ -25,7 +28,12 @@ export class NewUserComponent implements OnInit {
       // The array's 3rd position is used for async validations
       userName: ['', [Validators.required, Validators.minLength(4), lowerCaseValidator], [this.userExistsService.userExists()]],
       password: ['', [Validators.required, Validators.minLength(6)]]
-    })
+    },
+    // The second group parameter contains the form validations
+    {
+      validators: [userEqualsPasswordValidator]
+    }
+    )
   }
 
   register() {
