1df7d41ea80f65ad670f71ad35a6292a1adbbbc4
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-10 13:59:24 -0300

    User registration

--- src/app/home/new-user/new-user.component.ts
+++ src/app/home/new-user/new-user.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { UserExistsService } from './user-exists.service';
 import { NewUserService } from './new-user.service';
 import { Component, OnInit } from '@angular/core';
@@ -15,7 +16,11 @@ export class NewUserComponent implements OnInit {
 
   newUserForm!: FormGroup
 
-  constructor(private formBuilder: FormBuilder, private newUserService: NewUserService, private userExistsService: UserExistsService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private newUserService: NewUserService,
+    private userExistsService: UserExistsService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.newUserForm = this.formBuilder.group(
@@ -37,8 +42,25 @@ export class NewUserComponent implements OnInit {
   }
 
   register() {
-    const newUser = this.newUserForm.getRawValue() as NewUser
-    console.log(newUser)
-  }
+    console.log('Entrou no método register')
+    if (this.newUserForm.valid) {
+      console.log('Entrou no if')
+      const newUser = this.newUserForm.getRawValue() as NewUser
+      this.newUserService.createNewUser(newUser).subscribe(
+        // Success function
+        () => {
+          console.log('Entrou na função de sucesso')
+          this.router.navigate([''])
+        },
+        // Error function
+        (error) => {
+          console.log('Entrou na função de erro')
+          console.log(error)
+        }
+     )
+    } else {
+      console.log(this.newUserForm)
+    }
 
+  }
 }
