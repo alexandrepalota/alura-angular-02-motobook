20e5be28c7a9c2d51175908e184d38d67ef3bcb7
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-07-13 07:15:33 -0300

    Bikes service

--- src/app/bikes/bikes.service.ts
+++ src/app/bikes/bikes.service.ts
@@ -1,9 +1,23 @@
+import { TokenService } from './../authentication/token.service';
+import { Bikes } from './bikes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const API = 'http://localhost:3000'
 
 @Injectable({
   providedIn: 'root'
 })
 export class BikesService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private tokenService: TokenService) { }
+
+  listFromUser(userName: string): Observable<Bikes> {
+    const token = this.tokenService.getToken()
+    const headers = new HttpHeaders().append('x-access-token', token)
+    return this.httpClient.get<Bikes>(`${API}/${userName}/photos`, {
+      headers
+    });
+  }
 }
